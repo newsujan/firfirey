13dcc7ac5b7db2db0a4e1308d65a5c04a456c0bb
newsujan <newsujan@yahoo.com>
2017-11-28 17:01:12 +0545

    added profile page and url

--- firfirey/urls.py
+++ firfirey/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),url(r'^$', views.home, name="home")
+    url(r'^admin/', admin.site.urls),url(r'^$', views.home, name="home"),
+    url(r'^profile/', views.profile, name="profile"),
 ]
--- firfirey/views.py
+++ firfirey/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render  
+from django.shortcuts import render
+from django.views.generic import View
+
+
 def home(request):
 	template="index.html"
 	context={}
-	return render(request, template, context)  
+	return render(request, template, context)
+
+def profile(request):
+    return render(request, 'profile.html',{})
